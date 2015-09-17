2325dbbb3345e213d5757f656ff9edaf6dd4e219
Ron Ballouz <ronald.ballouz@gmail.com>
2015-09-17 00:32:18 -0400

    Implement all the trigger events and required classes/methods

--- Assets/Scripts/ShapeGlow.cs
+++ Assets/Scripts/ShapeGlow.cs
@@ -6,7 +6,7 @@ using System.Collections;
 public class ShapeGlow : MonoBehaviour {
 	private Vector3 startingPosition;
 	private Vector3 startingScale;
-
+	
 	void Start () {
 		startingPosition = transform.localPosition;
 		startingScale = transform.localScale;
@@ -21,5 +21,32 @@ public class ShapeGlow : MonoBehaviour {
 		h.enabled = gazedAt ? true : false;
 		// Allow Click Button to Animate
 		// ShowText
+	}
+	
+	public void StartOrbit() {
+		GameObject camera = GameObject.Find("CardboardMain");
+		TriggerMotion camera_trigger = camera.GetComponent<TriggerMotion> ();
+		camera_trigger.orbitY = false;
+		Gravity rosetta_physics = GetComponent<Gravity>();
+		rosetta_physics.position = new Vector3(11.15f, 6.93f, -0.53f);
+		rosetta_physics.velocity = new Vector3 (0f, 1f, 0f); //should be a function of its position?
+		rosetta_physics.enabled = true; //starts disabled 
+		
+	}
+	
+	public void StartLanding() {
+		GameObject camera = GameObject.Find("CardboardMain");
+		TriggerMotion camera_trigger = camera.GetComponent<TriggerMotion> ();
+		camera_trigger.orbitY = false;
+		PhilaeLanding philae_land = GetComponent<PhilaeLanding> ();
+		//GameObject camera = GameObject.FindGameObjectsWithTag ("CardboardMain");
+		//GameObject comet = GameObject.Find("67P");
+		//TriggerMotion comet_trigger = comet.GetComponent<TriggerMotion> ();
+		//comet.transform.localPosition = new Vector3(0f, 0f, 0f);
+		//comet.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+		//comet_trigger.enabled = false;
+		philae_land.position = new Vector3(-11.7f, 3.59f, -2.49f);
+		philae_land.velocity = new Vector3 (+2f, 0f, 0f);
+		philae_land.enabled = true;
 	}
 }
---
+++ Assets/Scripts/TriggerMotion.cs
@@ -0,0 +1,31 @@
+﻿// Script that turns on and off the rotational behavior of 67P
+using UnityEngine;
+using System.Collections;
+
+public class TriggerMotion : MonoBehaviour {
+	
+	public GameObject target = null;
+	public bool orbitY;
+	public Cardboard cb;
+	
+	// Cardboard.CardboardTriggered = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cb.Triggered) {
+			if (orbitY == false) {
+				orbitY = true;
+			}
+			else {
+				orbitY = false;
+			}
+		}
+		if (orbitY){
+			transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime * 10);	
+		}
+	}
+}
