706ee8afe2cd53c168f15ca91681c78b3b72e147
Ron Ballouz <ronald.ballouz@gmail.com>
2015-09-16 23:16:52 -0400

    Enlarge and glow the spacecrafts if gazing

---
+++ Assets/Scripts/ShapeGlow.cs
@@ -0,0 +1,25 @@
+﻿// This is a collection of Effects that modify the behavior of Rosetta and Philae
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class ShapeGlow : MonoBehaviour {
+	private Vector3 startingPosition;
+	private Vector3 startingScale;
+
+	void Start () {
+		startingPosition = transform.localPosition;
+		startingScale = transform.localScale;
+		SetGazedAt(false);
+	}
+	
+	public void SetGazedAt(bool gazedAt) {
+		// Enlarge
+		GetComponent<Transform>().localScale = gazedAt ? 3*startingScale : startingScale;
+		// Glow
+		Behaviour h = (Behaviour)GetComponent("Halo");
+		h.enabled = gazedAt ? true : false;
+		// Allow Click Button to Animate
+		// ShowText
+	}
+}
