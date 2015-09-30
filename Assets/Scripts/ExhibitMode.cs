ae1fca21f90e7835a15d35103baca061477aa82c
Chi-kwan Chan <chanc@email.arizona.edu>
2015-09-30 09:54:33 -0400

    Start implementing an exhibit mode like in the Google Cardboard app

---
+++ Assets/Scripts/ExhibitMode.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExhibitMode : MonoBehaviour {
+
+	public GameObject camera = null;
+	public GameObject target = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
