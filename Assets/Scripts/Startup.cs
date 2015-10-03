8a8c0bde1e862d0a4683d4de89fcfd1ae4e73d16
Chi-kwan Chan <chanc@email.arizona.edu>
2015-10-02 23:16:55 -0400

    Rename "Start" to "Startup"; add a script to remove the startup screen

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startup : MonoBehaviour {
+
+	public Cardboard  Handle = null;
+	public GameObject Screen = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Handle.Triggered && Screen) {
+			Destroy (Screen);
+			Screen = null;
+		}
+	}
+}
