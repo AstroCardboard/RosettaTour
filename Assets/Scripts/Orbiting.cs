e1fce9844a0e1759080604def69b0818a8147d7b
Chi-kwan Chan <chanc@email.arizona.edu>
2015-10-04 22:24:10 -0400

    Add Orbiting.cs; make the camera and spacecrafts orbit around 67P

---
+++ Assets/Scripts/Orbiting.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbiting : MonoBehaviour {
+
+	public GameObject center = null;
+	const  float      speed  = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(center)
+			transform.RotateAround(center.transform.position, Vector3.up, speed * Time.deltaTime);
+	}
+}
