b7607ec4fe3d65424b8283bdf160202efcdb77e0
Chi-kwan Chan <chanc@email.arizona.edu>
2016-03-30 13:54:54 -0400

    Cycling through slides is now functional

---
+++ Assets/Scripts/Present.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Present : MonoBehaviour {
+
+	private int current = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Next (Material mat) {
+		if (++current >= 5)
+			current = 0;
+
+		string file = "slides" + current.ToString ("D2") + "Tex";
+		mat.mainTexture = Resources.Load (file) as Texture;
+	}
+}
