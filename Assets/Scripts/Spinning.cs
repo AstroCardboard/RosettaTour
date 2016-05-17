a47d5261f691eb523cd6b3c7247a2b234d93bb3d
Chi-kwan Chan <chanc@email.arizona.edu>
2016-05-17 11:02:38 -0400

    Spin 67P instead of letting other objects orbit around it

---
+++ Assets/Scripts/Spinning.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinning : MonoBehaviour {
+
+	const   float speed   = 10.0f;
+	private bool  running = true;
+
+	// Update is called once per frame
+	void Update () {
+		if (running)
+			transform.Rotate (-speed * Time.deltaTime * Vector3.right);
+	}
+
+	public void Pause () {
+		running = !running;
+	}
+}
